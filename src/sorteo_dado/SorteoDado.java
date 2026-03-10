07125b81397a5ce9a88770a9ca84ae3afb3677bd
Gera980 <25300730@uttt.edu.mx>
2026-03-10 11:39:39 -0600

    Agrega variables y el metodo lanzarDado

--- src/sorteo_dado/SorteoDado.java
+++ src/sorteo_dado/SorteoDado.java
@@ -9,5 +9,10 @@ package sorteo_dado;
  * @author jSarabia
  */
 public class SorteoDado {
-    
+	public static String dado;
+	public static int resultado;
+
+	public static void lanzarDado(){
+	
+	}    
 }

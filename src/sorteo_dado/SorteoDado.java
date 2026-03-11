167c46f9c94bd3cc19a790c706a13da0a9b65877
gerardo guerrero <gerardoguerrerozero@gmail.com>
2026-03-10 20:48:06 -0600

    Creacion de metodos y variables en el archivo SorteoDado hecho por Gerardo :)

--- src/sorteo_dado/SorteoDado.java
+++ src/sorteo_dado/SorteoDado.java
@@ -9,5 +9,33 @@ package sorteo_dado;
  * @author jSarabia
  */
 public class SorteoDado {
-    public static int dado;
+    // Hecho por Gerardo <3
+    public static String dado;
+    public static int iteracion;
+    
+
+    public static int lanzarDado() {
+        return (int) (Math.random() * 6) + 1;
+    }
+
+    public static String compararDado(int n){
+        int i = 1;
+        int d = 0;
+        String reporte = "";
+        
+        while(i <= n){
+            
+            d = lanzarDado();
+            
+            if (d == 1 || d == 2) {
+                reporte += "Iteración " + i + " \t " + " Dado: " + d + "\n";
+                i++;
+            }else if (d == 3 || d == 4 || d == 5 || d == 6){
+                reporte += "Iteración " + i + " \t " + "Dado: " + d + "\n";
+                break;
+            }
+        }
+        return reporte;
+    }
+    // Hecho por Gerardo <3
 }
